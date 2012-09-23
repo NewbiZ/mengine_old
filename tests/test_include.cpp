783387355c20b991182b95531848b34df6aa6033
Aurélien Vallée <vallee.aurelien@gmail.com>
2012-09-24 01:41:52 +0200

    Add test build system

--- tests/test_include.cpp
+++ tests/test_include.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <cstdlib>
 
+// Just check that we at least have the mengine entry point header
 #include <mengine/mengine.h>
 
 int main(int argc, char** argv)
 {
-    mengine::hello();
-
     return EXIT_SUCCESS;
 }
-
