5bb960707df500f6966227c993e9e75a6e3a025d
Aurélien Vallée <vallee.aurelien@gmail.com>
2012-09-25 01:23:45 +0200

    Fix library generation

--- libmengine/mengine.cpp
+++ libmengine/mengine.cpp
@@ -2,12 +2,38 @@
 
 #include <iostream>
 
+#include <GL/glfw.h>
+
 namespace mengine
 {
 
 void hello()
 {
-    std::cout << "Hello world!" << std::endl;
+    bool running = false;
+
+    if (!glfwInit())
+    {
+        std::cerr << "error: failed to initialize glfw" << std::endl;
+        return;
+    }
+
+    if (!glfwOpenWindow(500, 500, 0, 0, 0, 0, 0, 0, GLFW_WINDOW))
+    {
+        std::cerr << "error: failed to open glfw window" << std::endl;
+        glfwTerminate();
+        return;
+    }
+
+    while (running)
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers();
+
+        running = glfwGetKey(GLFW_KEY_ESC) && glfwGetWindowParam(GLFW_OPENED);
+    }
+
+    glfwTerminate();
 }
 
 }
